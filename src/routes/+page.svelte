1e15962338115139641cee007ec67025b99b1074
Indeed <49447848+Indeedornot@users.noreply.github.com>
2022-11-19 12:39:18 +0100

    Updated to show problems with respecting conditional classes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,13 @@
 <script>
 	let innerHTML = 'Hello World';
+	export let test = false;
 </script>
 
-<h1 bind:innerHTML={innerHTML} contenteditable="true" />
-<h1 innerHTML={innerHTML}/>
+<h1 bind:innerHTML={innerHTML} contenteditable="true" class:test={test} />
+<h1 innerHTML={innerHTML} class:test={test}/>
+
+<style>
+	.test{
+		display: none;
+	}
+</style>
